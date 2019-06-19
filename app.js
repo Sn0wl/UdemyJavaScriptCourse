aacad51522e5a81faf47b0107d17784d896b00e1
Daniel Woche <daniel-132@hotmail.de>
2019-06-19 14:44:01 +0200

    Initial Training Files

---
+++ app.js
@@ -0,0 +1,8 @@
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+val = list.childNodes;
+
+console.log(val);
